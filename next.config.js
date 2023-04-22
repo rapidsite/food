93ebaefc78f18b6a3bdb50aaa7fcfd1ffed00ec3
Rapidsite <128565288+rapidsite@users.noreply.github.com>
2023-04-22 10:48:07 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,24 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-// next.config.js
-/** @type {import('next').NextConfig} */
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-    }
-  }
-
-  // Production config
-  return {
-    swcMinify: true,
-    images: {
-      loader: 'custom',
-      loaderFile: './cfImageLoader.js'
-    },
-  }
-}
